719b35dd680889dae9f32b01547c7e6470ecd662
Ravi Parmar <ravi.parmar@zebpay.com>
2018-09-10 16:18:38 +0530

    commit

---
+++ CommunicationHandaler/CommunicationHandaler.swift
@@ -0,0 +1,14 @@
+//
+//  CommunicationHandaler.swift
+//  FBSnapshotTestCase
+//
+//  Created by Ravi Parmar on 10/09/18.
+//
+
+import Foundation
+
+public class CommunicationHandler: NSObject{
+    public func PrintIt(){
+        print("Done with pod integration")
+    }
+}
